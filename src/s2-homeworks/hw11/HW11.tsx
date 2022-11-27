7d120369560966e10fbf1f267dea10910b099800
Sergey Saprankov <sergey-saprankov@mail.ru>
2022-11-27 18:11:45 +0300

    HW11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -3,6 +3,7 @@ import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
 import { restoreState } from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
+import {logDOM} from "@testing-library/react";
 
 /*
 * 1 - передать значения в оба слайдера
@@ -15,8 +16,14 @@ function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
+    const change = (event: Event, value: number | number[]) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+        if ( Array.isArray(value)) {
+            setValue1(value[0])
+            setValue2(value[1])
+        } else {
+            setValue1(value)
+        }
     }
 
     return (
@@ -29,16 +36,16 @@ function HW11() {
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
-                            // сделать так чтоб value1 изменялось // пишет студент
-
+                            onChange={change}
+                            value={value1}
                         />
                     </div>
                     <div className={s.wrapper}>
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
-                            // сделать так чтоб value1/2 изменялось // пишет студент
-
+                            onChange={change}
+                            value={[value1, value2]}
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,7 +5,49 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                color: '#00CC72',
+                height: 8,
+                '& .MuiSlider-track': {
+                    border: 'none',
+                },
+                '& .MuiSlider-thumb': {
+                    height: 24,
+                    width: 24,
+                    backgroundColor: '#fff',
+                    border: '2px solid currentColor',
+                    '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+                        boxShadow: 'inherit',
+                    },
+                    '&:before': {
+                        display: 'none',
+                    },
+                },
+                '& .MuiSlider-thumb: after': {
+                    width: "6px",
+                    height: "6px",
+                    top: "50%",
+                    left: "50%",
+                    backgroundColor: '#52af77',
+                }  ,
+                '& .MuiSlider-valueLabel': {
+                    lineHeight: 1.2,
+                    fontSize: 12,
+                    background: 'unset',
+                    padding: 0,
+                    width: 32,
+                    height: 32,
+                    borderRadius: '50% 50% 50% 0',
+                    backgroundColor: '#52af77',
+                    transformOrigin: 'bottom left',
+                    transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
+                    '&:before': { display: 'none' },
+                    '&.MuiSlider-valueLabelOpen': {
+                        transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
+                    },
+                    '& > *': {
+                        transform: 'rotate(45deg)',
+                    },
+                },
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
